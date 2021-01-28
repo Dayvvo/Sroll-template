f461578aaa47a3683d100b8b41b8cc5bfddf9846
Dayvvo <dyvvoo@gmail.com>
2021-01-28 11:00:55 +0100

    'First-commit'

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+let scroller = document.querySelector('#scroll')
+scroller.addEventListener('click',(e)=>{
+    e.preventDefault()
+    let scroller = document.querySelector('.slidebar');
+    scroller.classList.toggle('slidenow');
+
+})
+('#scroll').click( function(e) {
+} )
+
